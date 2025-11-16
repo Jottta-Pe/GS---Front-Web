4516f392b4d711e7705aaa8c5f3b53e4f4c656ae
Jottta-Pe <cerberus.sixxx@gmail.com>
2025-11-16 01:30:55 -0300

    darkmode beta

---
+++ Front-Web-Gs/src/App.jsx
@@ -0,0 +1,51 @@
+import { Moon, Sun } from "lucide-react";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [theme, setTheme] = useState("light");
+
+  // Alternar tema
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+
+    document.documentElement.classList.toggle("dark", newTheme === "dark");
+    localStorage.setItem("theme", newTheme);
+  };
+
+  // Carregar tema salvo
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme") || "light";
+    setTheme(storedTheme);
+
+    document.documentElement.classList.toggle("dark", storedTheme === "dark");
+  }, []);
+
+  return (
+    <div className="h-full min-h-screen flex flex-col items-center justify-center transition-all ease-in-out dark:bg-gray-700">
+
+      {/* 🔥 BOTÃO FIXADO NO CANTO SUPERIOR DIREITO */}
+      <button
+        onClick={toggleTheme}
+        className="fixed top-4 right-4 z-50
+                   rounded-full p-2 shadow-md
+                   shadow-purple-400 text-gray-700 hover:text-purple-500
+                   transition-all ease-in-out cursor-pointer
+                   bg-white
+                   dark:bg-gray-800 dark:text-yellow-300 dark:hover:text-yellow-400 dark:shadow-amber-400"
+      >
+        {theme === "light" ? <Moon size={22} /> : <Sun size={22} />}
+      </button>
+
+      {/* Mensagem no centro */}
+      <div className="text-lg dark:text-white mt-10">
+        {theme === "light"
+          ? "🌞 Bright mode activated"
+          : "🌙 Welcome to the dark side"}
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
